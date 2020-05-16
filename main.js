d6a9e7c0e768c3be9fa76bf4f33b2a11faae096c
Adam Poznar <adampoznar@gmail.com>
2020-05-16 16:52:26 +0200

    BEM changes

--- main.js
+++ main.js
@@ -4,4 +4,5 @@ const age = 29;
 console.log(`Cześć, nazywam się ${firstName} i mam ${age} lat :)`);
 
 const footer = document.querySelector('.footer');
+
 footer.innerHTML = 'Są to podstawy frontend stworzone przy pomocy kursu WTF Macieja Korsana. Ten element został wyświetlony za pomocą DOM';

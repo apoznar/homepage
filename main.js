a1dd5268abed4a7d839e10ffbbfe3729b6d97351
Adam Poznar <adampoznar@gmail.com>
2020-05-23 17:17:35 +0200

    Added JS function example

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const firstName = "Adam";
 const age = 29;
 
-console.log(`Cześć, nazywam się ${firstName} i mam ${age} lat :)`);
+const welcome = (name, age) => {
+    console.log(`Cześć, nazywam się ${name} i mam ${age} lat :) Ta linia jest efektem działania funkcji.`);
+}
+
+welcome(firstName, age);
+
+console.log(`Cześć, nazywam się ${firstName} i mam ${age} lat :) Ta linia jest efektem wyświetlania zmiennych w łańcuchu znaków.`);
 
 const footer = document.querySelector('.footer');
 

ba7240e327c8909148dbd74b7a39b5b20ff13e3b
Adam Poznar <adampoznar@gmail.com>
2020-05-27 17:49:48 +0200

    hamburger menu & media queries

--- main.js
+++ main.js
@@ -2,13 +2,25 @@ const firstName = "Adam";
 const age = 29;
 
 const welcome = (name, age) => {
-    console.log(`Cześć, nazywam się ${name} i mam ${age} lat :) Ta linia jest efektem działania funkcji.`);
-}
+  console.log(
+    `Cześć, nazywam się ${name} i mam ${age} lat :) Ta linia jest efektem działania funkcji.`
+  );
+};
 
 welcome(firstName, age);
 
-console.log(`Cześć, nazywam się ${firstName} i mam ${age} lat :) Ta linia jest efektem wyświetlania zmiennych w łańcuchu znaków.`);
+console.log(
+  `Cześć, nazywam się ${firstName} i mam ${age} lat :) Ta linia jest efektem wyświetlania zmiennych w łańcuchu znaków.`
+);
 
-const footer = document.querySelector('.footer');
+const footer = document.querySelector(".footer");
 
-footer.innerHTML = 'Są to podstawy frontend stworzone przy pomocy kursu WTF Macieja Korsana. Ten element został wyświetlony za pomocą DOM';
+footer.innerHTML =
+  "Są to podstawy frontend stworzone przy pomocy kursu WTF Macieja Korsana. Ten element został wyświetlony za pomocą DOM";
+
+const hamburger = document.querySelector(".hamburger--js");
+
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector(".navigation--js");
+  nav.classList.toggle("navigation--open");
+});
